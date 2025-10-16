559351131d2d588569d0310499999c92a4d3d2e7
rednaxela1813 <rednaxela1813@gmail.com>
2025-10-16 16:32:49 +0200

    create news/models.py: NewsSource, Article, Digest, DigestArticle^ created created admin.py

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0hy5j#lghuq-t)m^^xb*#)zht^_qkt3^3&+bi_(wlz%o^9@nai'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Bratislava'
 
 USE_I18N = True
 
--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import NewsSource, Article, Digest, DigestArticle
 
-# Register your models here.
+
+
+@admin.register(NewsSource)
+class NewsSourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rss_url', 'active', 'created_at')
+    list_filter = ('active',)
+    search_fields = ('name', 'rss_url')
+    
+    
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source', 'published', 'link')
+    list_filter = ('source',)
+    search_fields = ('title', 'summary', 'link')
+    date_hierarchy = 'published'
+    
+    
+class DigestArticleInline(admin.TabularInline):
+    model = DigestArticle
+    extra = 1
+    autocomplete_fields = ('article',)
+    
+    
+@admin.register(Digest)
+class DigestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    #date_hierarchy = 'created_at'
+    inlines = [DigestArticleInline]
+    search_fields = ('name',)
+    
+    
+@admin.register(DigestArticle)
+class DigestArticleAdmin(admin.ModelAdmin):
+    list_display = ('digest', 'article')
+    search_fields = ('digest__name', 'article__title')
+    autocomplete_fields = ('digest', 'article')
